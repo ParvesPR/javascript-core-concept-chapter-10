d20084e11811dd63044b9c81724be452db4c4db0
ParvesPR <parves2585@gmail.com>
2024-09-01 13:32:55 +0800

    basic object literal in OOP

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert('Hello World')
+// Object Literal
+var rect= {
+    width: 100,
+    height: 50,
+
+    draw: function () {
+        console.log('I am a rectangle');
+        console.log('My width is '+ this.width);
+        console.log('My height is '+ this.height);
+    }
+}
+rect.draw()
+rect.width=110;
+rect.height=70;
+rect.draw()
