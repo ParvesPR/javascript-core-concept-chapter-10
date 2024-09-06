f76e4096e45dc070add8cee664e528207be10af5
ParvesPR <parves2585@gmail.com>
2024-09-06 15:40:52 +0800

    passing THIS method to another object

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 // Object Literal
-var rect= {
+// var rect = {
+//     width: 100,
+//     height: 50,
+
+//     draw: function () {
+//         console.log('I am a rectangle');
+//         console.log('My width is ' + this.width);
+//         console.log('My height is ' + this.height);
+//     }
+// }
+// rect.draw()
+
+// THIS method
+var rect = {
     width: 100,
     height: 50,
 
     draw: function () {
         console.log('I am a rectangle');
-        console.log('My width is '+ this.width);
-        console.log('My height is '+ this.height);
+        this.print()
+    },
+
+    print: function () {
+        console.log('My width is ' + this.width);
+        console.log('My height is ' + this.height);
     }
 }
-rect.draw()
-rect.width=110;
-rect.height=70;
-rect.draw()
+rect.draw();
+
+var another = {
+    width: 75,
+    height: 30,
+    properties: rect.print
+}
+another.properties()
